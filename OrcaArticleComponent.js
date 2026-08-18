98621fec3006255c948daca8125d543f92386081
michael phillips <michael.phillips4@yahoo.co.uk>
2026-08-18 14:34:55 +0100

    added videos and better styling

--- OrcaArticleComponent.js
+++ OrcaArticleComponent.js
@@ -6,7 +6,9 @@ class OrcaArticleComponent extends HTMLElement {
     header.innerText = this.dataset.title;
     header.href = this.dataset.url;
     clone.querySelector("#content").innerHTML = this.innerHTML;
-   
+    const image = clone.querySelector(".app-image");
+    image.src = this.dataset.image;
+    image.alt = this.dataset.imageText
     this.innerHTML = "";
     this.appendChild(clone);
   }
